5ac98c0859a9e53630c1dd71bed17f3512525f92
Caio Yassoyama <caio.yassoyama@gmail.com>
2016-02-29 20:33:38 -0300

    Code after lecture 18.

--- weekdays/index.ios.js
+++ weekdays/index.ios.js
@@ -1,4 +1,5 @@
 // Import some code we need
+var Moment = require('moment');
 var React = require('react-native');
 var AppRegistry = React.AppRegistry;
 var View = React.View;
@@ -6,23 +7,24 @@ var Text = React.Text;
 var StyleSheet = React.StyleSheet;
 var DayItem = require('./src/day-item');
 
-var DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
 // Create React component
 var Weekdays = React.createClass({
   render: function() {
     return <View style={styles.container}>
-      <Text>
-        Days of the week:
-      </Text>
       {this.days()}
     </View>
   },
   days: function() {
-    return DAYS.map(function(day) {
-      // Called 7 times, one for each day of the week
-      return <DayItem day={day}/>
-    });
+    var dayItems = [];
+
+    for (var i = 0; i < 7; i++) {
+      var day = Moment().add(i, 'days').format('dddd');
+      dayItems.push(
+        <DayItem day={day} daysUntil={i}/>
+      );
+    }
+
+    return dayItems;
   }
 });
 
