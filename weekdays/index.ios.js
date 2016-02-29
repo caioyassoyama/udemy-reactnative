d8d8a4935c77199407fa5bbd6f38a04c0507ae90
Caio Yassoyama <caio.yassoyama@movile.com>
2016-02-29 20:03:25 -0300

    Code after lecture 10.

--- weekdays/index.ios.js
+++ weekdays/index.ios.js
@@ -3,11 +3,12 @@ var React = require('react-native');
 var AppRegistry = React.AppRegistry;
 var View = React.View;
 var Text = React.Text;
+var StyleSheet = React.StyleSheet;
 
-// Create react component
+// Create React component
 var Weekdays = React.createClass({
   render: function() {
-    return <View>
+    return <View style={styles.container}>
       <Text>
         Days of the week:
       </Text>
@@ -15,7 +16,16 @@ var Weekdays = React.createClass({
   }
 });
 
-// Show the react component on the screen
+// Style the React component
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+// Show the React component on the screen
 AppRegistry.registerComponent('weekdays', function() {
   return Weekdays
 });
