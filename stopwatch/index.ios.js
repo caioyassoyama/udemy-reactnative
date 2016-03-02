e1e7d44b79b11560ae1925fb693c0e895a809195
Caio Yassoyama <caio.yassoyama@gmail.com>
2016-03-01 23:35:03 -0300

    Code after lecture 24.

---
+++ stopwatch/index.ios.js
@@ -0,0 +1,72 @@
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class StopWatch extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={[styles.header, this.border('yellow')]}>
+          <View style={this.border('red')}>
+            <Text>
+              00:00.00
+            </Text>
+          </View>
+          <View style={this.border('green')}>
+            {this.startStopButton()}
+            {this.lapButton()}
+          </View>
+        </View>
+        <View style={[styles.footer, this.border('blue')]}>
+          <Text>
+            I am a list of laps
+          </Text>
+        </View>
+      </View>
+    );
+  }
+  startStopButton() {
+    return (
+      <View>
+        <Text>
+          Start
+        </Text>
+      </View>
+    );
+  }
+  lapButton() {
+    return (
+      <View>
+        <Text>
+          Lap
+        </Text>
+      </View>
+    );
+  }
+  border(color) {
+    return {
+      borderColor: color,
+      borderWidth: 4
+    };
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch'
+  },
+  header: {
+    flex: 1
+  },
+  footer: {
+    flex: 1
+  }
+});
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
